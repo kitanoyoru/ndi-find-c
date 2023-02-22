7c48c801632c8b7f4a1674f30944bafcdf9343b2
Alexandr Rutkovskij <kitanoyoru@protonmail.com>
2023-02-22 16:39:37 +0300

    Init

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#include <Processing.NDI.Lib.h>
+
+int main() {
+    if (!NDIlib_initialize()) {
+        return 0;
+    }
+
+    const NDIlib_find_create_t NDI_find_create_desc;
+    NDIlib_find_instance_t pNDI_find = NDIlib_find_create_v2(&NDIlib_find_create_desc)
+    if (!pNDI_find) {
+        return 0;
+    }
+
+    uint32_t no_sources = 0;
+	const NDIlib_source_t* p_sources = NULL;
+    while(1) {
+        if (!NDIlib_find_wait_for_sources(pNDI_find, 1000)) {
+            continue;
+        };
+        p_sources = NDIlib_find_get_current_sources(pNDI_find, &no_sources);
+        for (uint32_t i = 0; i < no_sources; ++i) {
+            printf("%s", p->ndi_name);
+        }
+    }
+
+    NDIlib_find_destroy(pNDI_find);
+
+    NDIlib_destroy();
+
+    return 0;
+}
